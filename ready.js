23ac4801d94a1ded9d2dfbc3917aeafa06c8ef67
Peter Lilja-Sheeley <pliljasheeley@gmail.com>
2016-03-22 11:49:07 -0500

    changes

--- ready.js
+++ ready.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
     $("#myButton").click(function(){
-    $("#revealDiv").show();
-});
-});
+    $("#revealDiv").fadeIn(1000);
+    var div = $("#contact");
+    div.animate({top: '100px'}, "slow");
+    div.animate({fontSize: '1.5em'}, "slow");
+}); 
+}); 
+
